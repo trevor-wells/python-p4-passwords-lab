b53020b76bd29c3adce0107a1454f12a0ce4d3c5
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-29 20:17:56 -0600

    what

--- server/app.py
+++ server/app.py
@@ -31,16 +31,25 @@ class CheckSession(Resource):
     
     def get(self):
         if session['user_id']:
-            user = User.query.filter(User.id == session.get('user_id').first())
+            user = User.query.filter(User.id == session['user_id']).first()
             return user, 200
         return {}, 204
 
 class Login(Resource):
     
     def post(self):
-        [username, password] = request.get_json()
-        password_hash = bcrypt
-        user = User.query.filter(User.username == username, User.password_hash)
+
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': '401 Unauthorized'}, 401
 
 class Logout(Resource):
     
@@ -48,11 +57,12 @@ class Logout(Resource):
         session['user_id'] = None
         return {}, 204
 
-api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
-api.add_resource(ClearSession, '/clear', endpoint='clear')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
