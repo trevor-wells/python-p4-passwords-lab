c3f99b2de8433eb40fde9550bee0f79156cd7a1d
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-29 20:12:43 -0600

    hm

--- server/app.py
+++ server/app.py
@@ -28,16 +28,31 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        if session['user_id']:
+            user = User.query.filter(User.id == session.get('user_id').first())
+            return user, 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        [username, password] = request.get_json()
+        password_hash = bcrypt
+        user = User.query.filter(User.username == username, User.password_hash)
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
-api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(ClearSession, '/clear', endpoint='clear')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
